7c65da26dc81c0080b07e107ddfb7cbd53583e2b
camigomez35 <camigomez@experimentality.co>
2017-09-16 02:04:11 -0500

    Initial Commit

---
+++ PionerasApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PionerasApp
+//
+//  Created by Maria Camila  Gomez on 16/09/17.
+//  Copyright © 2017 Maria Camila  Gomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
