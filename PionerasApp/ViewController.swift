01a61196ba80c84bdb1cde2f355f98ff24b7bfc0
camigomez35 <camigomez@experimentality.co>
2017-09-16 02:36:32 -0500

    Creando botón que suma

--- PionerasApp/ViewController.swift
+++ PionerasApp/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensaje: UITextField!
+    
+    var contador = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        mensaje.text = String(contador)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func verHora(_ sender: UIButton) {
+        contador += 1
+        mensaje.text = String(contador)
+    }
 
 
 }
-
